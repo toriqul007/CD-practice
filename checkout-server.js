36c12d538208259f3730342cef498524c4b1a60a
Toriqul Islam <shohan.hkr@gmail.com>
2022-05-04 22:05:16 +0200

    live flow added in checkout-server

--- checkout-server.js
+++ checkout-server.js
@@ -5,6 +5,22 @@ const http = require('http');
 // command line/bash commands
 const {execSync} = require('child_process');
 
+// Path to db-template
+const dbTemplatePath = path.join(__dirname, 'database', 'products-template.db');
+const dbPath = path.join(__dirname, 'database', 'products.db');
+
+
+
+//A funcction that does all necessary git checkout, cleanup etc
+function checkout(){
+    execSync('git pull');
+    execSync('npm install'); // install new npm modules mentioned in package.json
+    //execSync('rm ' + dbPath); // remove the database
+    execSync('cp ' + dbTemplatePath + ' ' + dbPath); // copy dbTemplate to db
+    //execSync('npm run build'); // build the dist folder that will be served
+    execSync('pm2 restart main'); // restart our main app
+    console.log('Pulled, copied db and restarted the server');
+}
 
 // Set up a small server that only check out things if know
 // the secret hash
@@ -12,13 +28,8 @@ const server = http.createServer(function (req, res) {
   res.end('Ok');
   if (req.url === '/d56e562c0606f3bbaf01dabffe8a0373410b77469763860fc1961fb64376889c'
   ) {
-    // do a git pull
-    execSync('git pull');
-    console.log('has run git pull');
+    checkout();
   }
-
-  
-
 });
 
 // Start up the server 
